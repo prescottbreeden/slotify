5c544213853460c22cea5d06cf234d3e2f920c36
prescottbreeden <prescott.breeden@gmail.com>
2018-08-03 18:16:34 -0700

    DB connected

--- register.php
+++ register.php
@@ -1,17 +1,20 @@
 <?php
-	include('includes/classes/Constants.php');
-	include('includes/classes/Account.php');
 
-	$account = new Account();
+include('includes/config.php');
+include('includes/classes/Constants.php');
+include('includes/classes/Account.php');
 
-	include('includes/handlers/register-handler.php');
-	include('includes/handlers/login-handler.php');
+$account = new Account();
 
-	function getInputValue($name) {
-		if(isset($_POST[$name])) {
-			echo $_POST[$name];
-		}
+include('includes/handlers/register-handler.php');
+include('includes/handlers/login-handler.php');
+
+function getInputValue($name) {
+	if(isset($_POST[$name])) {
+		echo $_POST[$name];
 	}
+}
+
 ?>
 
 <!DOCTYPE html>
