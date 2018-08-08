b8dd79defe1bdc8ae88e9e1ea8889c47e0728b35
prescottbreeden <prescott.breeden@gmail.com>
2018-08-08 16:20:05 -0700

    small tweaks on top-bar hover states and spacing, added headers for main

--- includes/templates/main.php
+++ includes/templates/main.php
@@ -1,5 +1,6 @@
 <div class="main">
 	<div class="main__content">
-		main
+		<h2 class="main__content--heading">Browse</h2>
+		<h4 class="main__content--sub-heading">Soundtrack for your day</h4>
 	</div>
 </div>
--- includes/templates/top-bar.php
+++ includes/templates/top-bar.php
@@ -1,7 +1,7 @@
 <section class="top-bar">
 	<div class="top-bar__empty-space"></div>
-	<div class="top-bar__user-info">
+	<div title="Profile" class="top-bar__user-info">
 		<img class="top-bar__user-info--avatar" src="public/images/profile-pics/head_emerald.png" alt="user avatar">
-		<p class="top-bar__user-info--username">Chuck Norris</p>
+		<p class="top-bar__user-info--username">Prescott Breeden</p>
 	</div>
 </section>
