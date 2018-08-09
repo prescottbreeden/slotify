284065c8644b66d45b897e46d2a3819f8c6880d7
prescottbreeden <prescott.breeden@gmail.com>
2018-08-08 17:06:20 -0700

    album artwork loading

--- includes/templates/main.php
+++ includes/templates/main.php
@@ -3,4 +3,23 @@
 		<h2 class="main__content--heading">Browse</h2>
 		<h4 class="main__content--sub-heading">Soundtrack for your day</h4>
 	</div>
+	<div class="album__container">
+	
+<?php
+$albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND()");
+
+while($row = mysqli_fetch_array($albumQuery)) {
+
+	echo	"<div class='album__container--item'>
+				<img src='" . $row['artwork_path'] . "'>	
+				<div class='album__container--item-details'>
+					" . $row['title_name'] . "
+				</div>
+			</div>";
+
+
+}
+
+?>
+	</div>
 </div>
