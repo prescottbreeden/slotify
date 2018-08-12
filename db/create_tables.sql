86ce705ddf7e86530cfef001eb8ac3db97464148
prescottbreeden <prescott.breeden@gmail.com>
2018-08-11 17:13:03 -0700

    same as previous, some of the files didn't get sent with push

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -73,10 +73,10 @@ CREATE TABLE IF NOT EXISTS songs (
 			(name) 
 		VALUES
 			('Koji Kondo'),
-			('Nobuo Uematsu'),
-			('Mahito Yokota'),
 			('Takashi Tateishi'),
-			('Hirokazu Tanaka')
+			('Hirokazu Tanaka'),
+			('Nobuo Uematsu'),
+			('Mahito Yokota')
 			;
 
 --
@@ -97,7 +97,7 @@ CREATE TABLE IF NOT EXISTS songs (
 			('The Legend of Zelda', 1, 1, 'public/images/artwork/zelda.jpg'),
 			('Mega Man 2', 2, 1, 'public/images/artwork/megaman2.jpg'),
 			('Punch Out', 1, 1, 'public/images/artwork/punchout.jpg'),
-			('Metroid', 5, 1, 'public/images/artwork/metroid.jpg')
+			('Metroid', 3, 1, 'public/images/artwork/metroid.jpg')
 			
 			
 			;
@@ -109,44 +109,57 @@ CREATE TABLE IF NOT EXISTS songs (
  INSERT INTO songs 
 			(title_name, artist_id, album_id, genre_id, duration, song_path, album_order, play_count) 
 		VALUES
-			('Title Theme', 1, 1, 1, '3:00', 'public/music/zelda_1_title_theme.mp3', 1, 0),
-			('Menu Theme', 1, 1, 1, '3:01', 'public/music/zelda_2_menu_theme.mp3', 2, 0),
-			('Overworld Theme', 1, 1, 1, '3:00', 'public/music/zelda_3_overworld_theme.mp3', 3, 0),
-			('Dungeon Theme', 1, 1, 1, '3:00', 'public/music/zelda_4_dungeon_theme.mp3', 4, 0),
-			('Dungeon Clear', 1, 1, 1, '0:09', 'public/music/zelda_5_dungeon_clear.mp3', 5, 0),
-			('Receive Item', 1, 1, 1, '0:03', 'public/music/zelda_6_receive_item.mp3', 6, 0),
-			('Recorder Theme', 1, 1, 1, '0:04', 'public/music/zelda_7_recorder_theme.mp3', 7, 0),
-			('Life Lost', 1, 1, 1, '0:04', 'public/music/zelda_8_life_lost.mp3', 8, 0),
-			('Rescued', 1, 1, 1, '0:06', 'public/music/zelda_9_rescued.mp3', 9, 0),
-			('Ganon Dungeon', 1, 1, 1, '3:00', 'public/music/zelda_10_ganon_dungeon.mp3', 10, 0),
-			('Ganon Defeated', 1, 1, 1, '0:04', 'public/music/zelda_11_ganon_defeated.mp3', 11, 0),
-			('Ending Theme', 1, 1, 1, '3:00', 'public/music/zelda_12_ending_theme.mp3', 12, 0),
+			('Title Theme', 1, 1, 1, '3:00', 'public/music/nes_zelda/zelda_1_title_theme.mp3', 1, 0),
+			('Menu Theme', 1, 1, 1, '3:01', 'public/music/nes_zelda/zelda_2_menu_theme.mp3', 2, 0),
+			('Overworld Theme', 1, 1, 1, '3:00', 'public/music/nes_zelda/zelda_3_overworld_theme.mp3', 3, 0),
+			('Dungeon Theme', 1, 1, 1, '3:00', 'public/music/nes_zelda/zelda_4_dungeon_theme.mp3', 4, 0),
+			('Dungeon Clear', 1, 1, 1, '0:09', 'public/music/nes_zelda/zelda_5_dungeon_clear.mp3', 5, 0),
+			('Receive Item', 1, 1, 1, '0:03', 'public/music/nes_zelda/zelda_6_receive_item.mp3', 6, 0),
+			('Recorder Theme', 1, 1, 1, '0:04', 'public/music/nes_zelda/zelda_7_recorder_theme.mp3', 7, 0),
+			('Life Lost', 1, 1, 1, '0:04', 'public/music/nes_zelda/zelda_8_life_lost.mp3', 8, 0),
+			('Rescued', 1, 1, 1, '0:06', 'public/music/nes_zelda/zelda_9_rescued.mp3', 9, 0),
+			('Ganon Dungeon', 1, 1, 1, '3:00', 'public/music/nes_zelda/zelda_10_ganon_dungeon.mp3', 10, 0),
+			('Ganon Defeated', 1, 1, 1, '0:04', 'public/music/nes_zelda/zelda_11_ganon_defeated.mp3', 11, 0),
+			('Ending Theme', 1, 1, 1, '3:00', 'public/music/nes_zelda/zelda_12_ending_theme.mp3', 12, 0),
 
-			('Introduction', 4, 2, 1, '0:43', 'public/music/megaman2_1_introduction.mp3', 1, 0),
-			('Title Screen', 4, 2, 1, '0:44', 'public/music/megaman2_2_title_screen.mp3', 2, 0),
-			('Password Screen', 4, 2, 1, '0:34', 'public/music/megaman2_3_password_screen.mp3', 3, 0),
-			('Stage Select', 4, 2, 1, '0:32', 'public/music/megaman2_4_stage_select.mp3', 4, 0),
-			('Enemy Chosen', 4, 2, 1, '0:08', 'public/music/megaman2_5_enemy_chosen.mp3', 5, 0),
-			('Quick Man', 4, 2, 1, '1:25', 'public/music/megaman2_6_quick_man.mp3', 6, 0),
-			('Metal Man', 4, 2, 1, '1:27', 'public/music/megaman2_7_metal_man.mp3', 7, 0),
-			('Bubble Man', 4, 2, 1, '1:23', 'public/music/megaman2_8_bubble_man.mp3', 8, 0),
-			('Heat Man', 4, 2, 1, '1:01', 'public/music/megaman2_9_heat_man.mp3', 9, 0),
-			('Wood Man', 4, 2, 1, '1:21', 'public/music/megaman2_10_wood_man.mp3', 10, 0),
-			('Air Man', 4, 2, 1, '1:40', 'public/music/megaman2_11_air_man.mp3', 11, 0),
-			('Crash Man', 4, 2, 1, '2:37', 'public/music/megaman2_12_crash_man.mp3', 12, 0),
-			('Flash Man', 4, 2, 1, '2:37', 'public/music/megaman2_13_flash_man.mp3', 13, 0),
-			('Boss Battle', 4, 2, 1, '2:37', 'public/music/megaman2_14_boss_battle.mp3', 14, 0),
-			('Victory', 4, 2, 1, '2:37', 'public/music/megaman2_15_victory.mp3', 15, 0),
-			('Weapons Ready', 4, 2, 1, '2:37', 'public/music/megaman2_16_weapons_ready.mp3', 16, 0),
-			('Dr Wily\'s Map', 4, 2, 1, '2:37', 'public/music/megaman2_17_wilys_map.mp3', 17, 0),
-			('Dr Wily\'s Castle', 4, 2, 1, '2:37', 'public/music/megaman2_18_wilys_castle.mp3', 18, 0),
-			('Dr Wily\'s Castle2', 4, 2, 1, '2:37', 'public/music/megaman2_19_wilys_castle2.mp3', 19, 0),
-			('Dr Wily Defeated', 4, 2, 1, '2:37', 'public/music/megaman2_20_wily_defeated.mp3', 20, 0),
-			('Epilogue', 4, 2, 1, '2:37', 'public/music/megaman2_21_epilogue.mp3', 21, 0),
-			('Credits', 4, 2, 1, '2:37', 'public/music/megaman2_22_credits.mp3', 22, 0),
-			('Game Over', 4, 2, 1, '2:37', 'public/music/megaman2_23_game_over.mp3', 23, 0),
+			('Introduction', 2, 2, 1, '0:43', 'public/music/nes_megaman2/megaman2_1_introduction.mp3', 1, 0),
+			('Title Screen', 2, 2, 1, '0:44', 'public/music/nes_megaman2/megaman2_2_title_screen.mp3', 2, 0),
+			('Password Screen', 2, 2, 1, '0:34', 'public/music/nes_megaman2/megaman2_3_password_screen.mp3', 3, 0),
+			('Stage Select', 2, 2, 1, '0:32', 'public/music/nes_megaman2/megaman2_4_stage_select.mp3', 4, 0),
+			('Enemy Chosen', 2, 2, 1, '0:08', 'public/music/nes_megaman2/megaman2_5_enemy_chosen.mp3', 5, 0),
+			('Quick Man', 2, 2, 1, '1:25', 'public/music/nes_megaman2/megaman2_6_quick_man.mp3', 6, 0),
+			('Metal Man', 2, 2, 1, '1:27', 'public/music/nes_megaman2/megaman2_7_metal_man.mp3', 7, 0),
+			('Bubble Man', 2, 2, 1, '1:23', 'public/music/nes_megaman2/megaman2_8_bubble_man.mp3', 8, 0),
+			('Heat Man', 2, 2, 1, '1:01', 'public/music/nes_megaman2/megaman2_9_heat_man.mp3', 9, 0),
+			('Wood Man', 2, 2, 1, '1:21', 'public/music/nes_megaman2/megaman2_10_wood_man.mp3', 10, 0),
+			('Air Man', 2, 2, 1, '1:40', 'public/music/nes_megaman2/megaman2_11_air_man.mp3', 11, 0),
+			('Crash Man', 2, 2, 1, '2:30', 'public/music/nes_megaman2/megaman2_12_crash_man.mp3', 12, 0),
+			('Flash Man', 2, 2, 1, '2:17', 'public/music/nes_megaman2/megaman2_13_flash_man.mp3', 13, 0),
+			('Boss Battle', 2, 2, 1, '0:42', 'public/music/nes_megaman2/megaman2_14_boss_battle.mp3', 14, 0),
+			('Victory', 2, 2, 1, '2:07', 'public/music/nes_megaman2/megaman2_15_victory.mp3', 15, 0),
+			('Weapons Ready', 2, 2, 1, '0:30', 'public/music/nes_megaman2/megaman2_16_weapons_ready.mp3', 16, 0),
+			('Dr Wily\'s Map', 2, 2, 1, '0:09', 'public/music/nes_megaman2/megaman2_17_wilys_map.mp3', 17, 0),
+			('Dr Wily\'s Castle', 2, 2, 1, '2:38', 'public/music/nes_megaman2/megaman2_18_wilys_castle.mp3', 18, 0),
+			('Dr Wily\'s Castle2', 2, 2, 1, '2:44', 'public/music/nes_megaman2/megaman2_19_wilys_castle2.mp3', 19, 0),
+			('Dr Wily Defeated', 2, 2, 1, '0:11', 'public/music/nes_megaman2/megaman2_20_wily_defeated.mp3', 20, 0),
+			('Epilogue', 2, 2, 1, '1:10', 'public/music/nes_megaman2/megaman2_21_epilogue.mp3', 21, 0),
+			('Credits', 2, 2, 1, '1:07', 'public/music/nes_megaman2/megaman2_22_credits.mp3', 22, 0),
+			('Game Over', 2, 2, 1, '0:06', 'public/music/nes_megaman2/megaman2_23_game_over.mp3', 23, 0),
 
-			('Fight Theme', 1, 3, 1, '3:00', 'public/music/punch_out_fight_theme.m4a', 1, 0)
+			('Fight Theme', 1, 3, 1, '3:00', 'public/music/nes_punchout/punch_out_fight_theme.m4a', 1, 0),
+
+			('Title', 3, 4, 1, '1:50', 'public/music/nes_metroid/metroid_1_title.mp3', 1, 0),
+			('Samus Entry', 3, 4, 1, '0:07', 'public/music/nes_metroid/metroid_2_samus_entry.mp3', 2, 0),
+			('Brinstar (Rock Stage)', 3, 4, 1, '1:46', 'public/music/nes_metroid/metroid_3_brinstar_rock_stage.mp3', 3, 0),
+			('Mini Boss Room I - Kraid', 3, 4, 1, '1:40', 'public/music/nes_metroid/metroid_4_mini_boss_room1_kraid.mp3', 4, 0),
+			('Norfair (Flame Stage)', 3, 4, 1, '1:20', 'public/music/nes_metroid/metroid_5_norfair_flame_stage.mp3', 5, 0),
+			('Mini Boss Room II - Ridley', 3, 4, 1, '1:01', 'public/music/nes_metroid/metroid_6_mini_boss_room2_ridley.mp3', 6, 0),
+			('Silence', 3, 4, 1, '0:29', 'public/music/nes_metroid/metroid_7_silence.mp3', 7, 0),
+			('Item Acquisition Jingle', 3, 4, 1, '0:06', 'public/music/nes_metroid/metroid_8_item_acquisition_jingle.mp3', 8, 0),
+			('Tourian (Base Stage)', 3, 4, 1, '0:34', 'public/music/nes_metroid/metroid_9_tourian_base_stage.mp3', 9, 0),
+			('The Lord of Zebes', 3, 4, 1, '0:34', 'public/music/nes_metroid/metroid_10_the_lord_of_zebes.mp3', 10, 0),
+			('Escape', 3, 4, 1, '1:27', 'public/music/nes_metroid/metroid_11_escape.mp3', 11, 0),
+			('Ending', 3, 4, 1, '2:16', 'public/music/nes_metroid/metroid_12_ending.mp3', 12, 0)
 
 
 			
--- includes/top-bar.php
+++ includes/top-bar.php
@@ -2,6 +2,6 @@
 	<div class="top-bar__empty-space"></div>
 	<div title="Profile" class="top-bar__user-info">
 		<img class="top-bar__user-info--avatar" src="public/images/profile-pics/head_emerald.png" alt="user avatar">
-		<p class="top-bar__user-info--username">Prescott Breeden</p>
+		<p class="top-bar__user-info--username"><?php echo $_SESSION['userLoggedIn']?></p>
 	</div>
 </section>
