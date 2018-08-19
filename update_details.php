2d1271a91afe7819806695150bfd86c9138eefc3
prescottbreeden <prescott.breeden@gmail.com>
2018-08-18 20:23:09 -0700

    removed forms from edit profile, the default browser behavior messed up error notification if an email was already in use

--- update_details.php
+++ update_details.php
@@ -17,55 +17,49 @@ else {
 	</div>
 	<div class="userdetails__container">
 		<h2 class="userdetails__secondary-heading">email</h2>
-		<form>
-			<input 
-				value="<?php echo $user->getEmail(); ?>"
-				name="email"
-				placeholder="Enter new email address"
-				class="userdetails__input" 
-				type="email">
-			<?php // echo $account->getError(Constants::$error_login_failed); ?>
-			<button 
-				onclick="updateEmail()"
-				class="userdetails__submit-btn">
-				Update Email		
-			</button>
-		</form>
+		<input 
+			value="<?php echo $user->getEmail(); ?>"
+			name="email"
+			placeholder="Enter new email address"
+			class="userdetails__input" 
+			type="email">
+		<?php // echo $account->getError(Constants::$error_login_failed); ?>
+		<button 
+			onclick="updateEmail()"
+			class="userdetails__submit-btn">
+			Update Email		
+		</button>
 	</div>
 	<div class="userdetails__container">
 		<div class="userdetails__container--pwd">
 			<div class="userdetails__container--pwd-block">
 				<h2 class="userdetails__secondary-heading">password</h2>
-				<form>
-					<input 
-						name="oldPassword"
-						placeholder="Enter current password"
-						class="userdetails__input" 
-						type="password">
-					<button class="userdetails__submit-btn">
-						Submit
-					</button>
-				</form>
+				<input 
+					name="oldPassword"
+					placeholder="Enter current password"
+					class="userdetails__input" 
+					type="password">
+				<button class="userdetails__submit-btn">
+					Submit
+				</button>
 			</div>
 			<div class="userdetails__container--pwd-block">
 				<h2 class="userdetails__secondary-heading">new password</h2>
-				<form>
-					<input 
-						name="newPassword1"
-						placeholder="Enter new password"
-						class="userdetails__input" 
-						type="password">
-					<br>
-					<input 
-						name="newPassword2"
-						placeholder="Confirm new password"
-						class="userdetails__input" 
-						type="password">
-					<?php // echo $account->getError(Constants::$error_login_failed); ?>
-					<button class="userdetails__submit-btn">
-						Update Password
-					</button>
-				</form>
+				<input 
+					name="newPassword1"
+					placeholder="Enter new password"
+					class="userdetails__input" 
+					type="password">
+				<br>
+				<input 
+					name="newPassword2"
+					placeholder="Confirm new password"
+					class="userdetails__input" 
+					type="password">
+				<?php // echo $account->getError(Constants::$error_login_failed); ?>
+				<button class="userdetails__submit-btn">
+					Update Password
+				</button>
 			</div>
 		</div>
 	</div>
